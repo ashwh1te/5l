0714884e0e72594b37d1be20bfab70c86be45808
ashwh1te <112275824+ashwh1te@users.noreply.github.com>
2023-10-16 21:46:21 +0300

    5.js

--- 5.js
+++ 5.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function onClick(e) {
-    const price = [200000, 130000, 19000000]
+    const price = [3.900.000, 6.700.000, 1.600.000]
     const item = document.getElementById('select1')
     const count = document.getElementById('Count1')
     let result = document.getElementById('result')
     if (/^\d+$/.test(count.value)) {
-        result.value = price[item.value] * count.value + 'RUB'
+        result.value = price[item.value] * count.value + '₽'
     }
     else {
         alert('для ввода доступны только числа')
